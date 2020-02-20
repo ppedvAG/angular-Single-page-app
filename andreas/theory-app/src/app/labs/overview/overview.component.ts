2648558ae3af269ad2db17c008505a5164602530
Andreas Fuhrich <andreas.fuhrich@jobrouter.com>
2020-02-20 14:30:07 +0100

    ngOnDestroy added for overview components

--- andreas/theory-app/src/app/labs/overview/overview.component.ts
+++ andreas/theory-app/src/app/labs/overview/overview.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.css']
 })
-export class OverviewComponent implements OnInit {
+export class OverviewComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  ngOnDestroy(): void {
+    console.log('Lab OverviewComponent destroy');
+  }
 
 }
--- andreas/theory-app/src/app/todos/todosoverview/todosoverview.component.ts
+++ andreas/theory-app/src/app/todos/todosoverview/todosoverview.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
 
 @Component({
   selector: 'app-todosoverview',
   templateUrl: './todosoverview.component.html',
   styleUrls: ['./todosoverview.component.css']
 })
-export class TodosoverviewComponent implements OnInit {
+export class TodosoverviewComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngOnDestroy(): void {
+    console.log('TodosoverviewComponent destroy');
+  }
 }
