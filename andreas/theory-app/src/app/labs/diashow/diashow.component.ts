4be0a0ade3bedd63f4bb15bd1cb8ebf59643d04c
Andreas Fuhrich <andreas.fuhrich@jobrouter.com>
2020-02-20 15:39:31 +0100

    added diashow component

---
+++ andreas/theory-app/src/app/labs/diashow/diashow.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, OnDestroy, OnInit } from "@angular/core";
+
+@Component({
+  selector: 'app-diashow',
+  templateUrl: './diashow.component.html',
+  styleUrls: ['./diashow.component.css']
+})
+export class DiashowComponent implements OnInit, OnDestroy {
+
+  @Input() images: string[] = [];
+  private imageInterval: number; // alternative NodeJS.Timeout for server side rendering
+  private imageIndex:number = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.imageInterval = setInterval(() => {
+      if(this.imageIndex < this.images.length-1) {
+        this.imageIndex++;
+      } else {
+        this.imageIndex = 0;
+      }
+    }, 1000);
+    console.log(this.images[this.imageIndex]);
+  }
+
+  public ngOnDestroy(): void {
+    if(this.imageInterval) {
+      clearInterval(this.imageInterval);
+    }
+  }
+
+}
