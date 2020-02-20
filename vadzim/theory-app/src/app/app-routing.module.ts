bbe7511d690de6b6c6f021e169756b4483994712
Vadzim Naumchyk <vadzimn@ppedv.de>
2020-02-20 13:43:55 +0100

    routing added

--- vadzim/theory-app/src/app/app-routing.module.ts
+++ vadzim/theory-app/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OverviewComponent } from './labs/overview/overview.component';
+import { TodosoverviewComponent } from './todos/todosoverview/todosoverview.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'labs', component: OverviewComponent},
+  { path: 'todos', component: TodosoverviewComponent},
+  { path: '**', redirectTo: '/', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
