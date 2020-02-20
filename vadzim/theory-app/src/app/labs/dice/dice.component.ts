61404eb8c147096737da8274f8f80611210fb1df
Vadzim Naumchyk <vadzimn@ppedv.de>
2020-02-20 11:42:08 +0100

    dice

---
+++ vadzim/theory-app/src/app/labs/dice/dice.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dice',
+  templateUrl: './dice.component.html',
+  styleUrls: ['./dice.component.css']
+})
+export class DiceComponent implements OnInit {
+
+  zahl: number;
+
+  constructor() { 
+    this.zahl = Math.ceil(Math.random()*6);
+  }
+
+  ngOnInit(): void {
+  }
+
+}
--- vadzim/theory-app/src/app/labs/time/time.component.ts
+++ vadzim/theory-app/src/app/labs/time/time.component.ts
@@ -7,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimeComponent implements OnInit {
 
-  constructor() { }
+  date: Date;
+  timestring: string;
+
+  constructor() {
+    setInterval(() => this.showTime(), 1000);
+   }
+
+  showTime(): void {
+    this.date = new Date();
+    this.timestring = this.date.toLocaleTimeString();
+  }
 
   ngOnInit(): void {
   }
