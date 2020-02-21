fee682e6d4d9a8db02d3faf37abad6a8dd1c9f7d
Vadzim Naumchyk <vadzimn@ppedv.de>
2020-02-21 13:17:04 +0100

    add todo form

--- vadzim/theory-app/src/app/todos/todos.module.ts
+++ vadzim/theory-app/src/app/todos/todos.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { TodosoverviewComponent } from './todosoverview/todosoverview.component';
 import { RatingComponent } from './rating/rating.component';
 import { TodotableComponent } from './todotable/todotable.component';
+import { AddFormComponent } from './add-form/add-form.component';
 
 @NgModule({
-  declarations: [TodosoverviewComponent, RatingComponent, TodotableComponent],
+  declarations: [TodosoverviewComponent, RatingComponent, TodotableComponent, AddFormComponent],
   imports: [
     CommonModule
   ],
